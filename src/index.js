39aa36f62cd61bf96c6e85fd9cbf9f3f35d1e85e
BianchoS <89320566+BianchoS@users.noreply.github.com>
2022-01-27 11:11:13 +0100

    installed bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Container from "./Container";
+import "bootstrap/dist/css/bootstrap.css";
 import Coder from "./Coder";
 
 import "./App.css";
