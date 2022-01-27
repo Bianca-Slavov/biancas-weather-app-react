9728805ea8af78a15538cca809344d650f8906c5
BianchoS <89320566+BianchoS@users.noreply.github.com>
2022-01-27 18:00:48 +0100

    worked on the API integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,30 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Weather.css";
+import { search } from "language-tags";
 
 export default function Weather() {
-  let weatherData = {
-    city: "Amsterdam",
-    temperature: 12,
-    date: "Friday",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-  };
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+      img: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+    });
+  }
 
-  return (
-    <div className="Temperature">
-      <h1>{weatherData.city}</h1>
-      <p className="current-date">{weatherData.date}</p>
-      <img
-        src={weatherData.imgUrl}
-        width="50px"
-        alt="weather-icon"
-        className="icon"
-      />
+  function search() {
+    const apiKey = "bad8df618c99f7689be26e10f430a853";
+    let city = "london";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-      <div className="weather-temperature">
-        <strong>{weatherData.temperature}</strong>
-        <span className="units">°C</span>
+  if (weatherData.ready) {
+    return (
+      <div className="Temperature">
+        <h1>{weatherData.city}</h1>
+        <p className="current-date">{weatherData.date}</p>
+        <img
+          src={weatherData.imgUrl}
+          width="50px"
+          alt="weather-icon"
+          className="icon"
+        />
+
+        <div className="weather-temperature">
+          <strong>{weatherData.temperature}</strong>
+          <span className="units">°C</span>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
