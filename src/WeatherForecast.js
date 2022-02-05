d58e54e0cfab86aca8463f0cf6655795348990b9
BianchoS <89320566+BianchoS@users.noreply.github.com>
2022-02-05 19:01:53 +0100

    Created a new component to show the weather forecast of the coming 5 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,3 +1,20 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
-import WeatherForecastDay from "./WeatherForecastDay";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <div className="row">
+        <div className="col">
+          <div className="WeatherForecast-date">Thu</div>
+          <WeatherIcon code="01d" size={36} />
+          <div className="WeatherForecast-temperatures">
+            <span className="WeatherForecast-temperature-max">19° </span>
+            <span className="WeatherForecast-temperature-min"> / 10°</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
