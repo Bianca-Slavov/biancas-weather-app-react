8222fe449c5870f78367500efca976d21157fd12
BianchoS <89320566+BianchoS@users.noreply.github.com>
2022-02-05 20:48:12 +0100

    Added Math.round on windspeed

--- src/Weather.js
+++ src/Weather.js
@@ -16,7 +16,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      windspeed: response.data.wind.speed,
+      windspeed: Math.round(response.data.wind.speed),
       city: response.data.name,
       icon: response.data.weather[0].icon,
     });
